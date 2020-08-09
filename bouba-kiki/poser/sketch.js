b55522c9a2cf7ebde6a540ceca34b03ac320ff05
jspong0 <jspong0@>
2020-08-09 17:10:58 +0300

    pose recorder for local files

--- bouba-kiki/poser/sketch.js
+++ bouba-kiki/poser/sketch.js
@@ -1,4 +1,3 @@
-let videoURL;
 let videoField;
 let videoButton;
 let video;
@@ -9,28 +8,52 @@ let poseHistory = [];
 
 let status;
 let rec = false;
-let recordLength = 800;
+let recordLength = 1000;
+let videoLocatorField;
+let videoSubmitButton;
+
+function setup() {
+	createCanvas(800,600);
+	status = select('#status');
+	videoLocatorField=createInput('') 
+	videoSubmitButton=createButton('Submit')
+	videoSubmitButton.mousePressed(getNewVideo)
+}
+
+function draw() {
+	background(225);
+
+	if (poses) {
+		if (poses[0]) {
+			let pose = poses[0];
+			if (rec) recordPose(pose);
+		}
+	}
+}
 
 /**
  * Load a new video, uses findSDVideo() to find the smallest file, and calls videoReady() and calls
  */
 function getNewVideo() {
+	let videoURL = videoLocatorField.value()
+	console.log(videoURL)
 
-	let newVideo = fetch(videoURL)
-		.then(response => {
-			if (!response.ok) {
-				throw new Error('http error: ' + response.status);
-			} else {
-				return response.json();
-			}
-		})
-		.then(result => {
-			video = createVideo(videoURL, videoReady);
-			video.loop();
-		})
-		.catch(e => {
-			console.log('fetch error: ' + e.message);
-		});
+	video = createVideo(videoURL, videoReady);
+	video.loop();
+
+	// let newVideo = fetch('videoURL')
+	// 	.then(response => {
+	// 		if (!response.ok) {
+	// 			throw new Error('http error: ' + response.status);
+	// 		} else {
+	// 			return response.json();
+	// 		}
+	// 	})
+	// 	.then(result => {
+	// 	})
+	// 	.catch(e => {
+	// 		console.log('fetch error: ' + e.message);
+	// 	});
 }
 
 /**
@@ -48,24 +71,6 @@ function modelReady() {
 	status.html('Model ready');
 }
 
-function setup() {
-	var canvas = createCanvas(windowWidth,windowHeight);
-	canvas.parent('canvas-placeholder');
-
-	status = select('#status');
-
-}
-
-function draw() {
-	background(225);
-
-	if (poses) {
-		if (poses[0]) {
-			let pose = poses[0];
-			if (rec) recordPose(pose);
-		}
-	}
-}
 
 function recordPose(points) {
 	poseHistory.push(points);
@@ -75,4 +80,8 @@ function recordPose(points) {
 function finishRecording() {
 	console.log('recording finished')
 	rec = false;
+	outputField = document.createElement('textarea')
+	document.getElementById('main').appendChild(outputField)
+outputField.value=JSON.stringify(poseHistory)
 }
+
